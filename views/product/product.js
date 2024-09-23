3b74435bfeb6ed49264252eaceadeef86f1ec7a9
Hung <jimmyhunganh2k@gmail.com>
2024-09-23 16:26:41 +0700

    mgk

--- views/product/product.js
+++ views/product/product.js
@@ -1,11 +1,28 @@
 app.controller("product", function ($scope, $http, $location, $filter) {
+    $scope.categories = [];
+    $scope.form = {};
 
 
+    $scope.initialize = function () {
+        // $scope.isLoading = true;
+        // // Load data 
+        // $http.get("http://localhost:8000/api/admin/products").then(resp => {
+        //     $scope.products = resp.data;
+        //     $scope.filterPro =resp.data;
+        //     console.log($scope.products);
+        // }).catch(error => {
+        //     console.log("Error", error);
+        // })
+
+       //Load data floors
+     $http.get("http://localhost:8000/api/admin/categories").then(resp => {
+        $scope.categories = resp.data;
+    }).catch(error => {
+        console.log("Error", error);
+    })
+    }
 
 
-
-
-
-
+    $scope.initialize();
 
 });
