ddc4737d0d3018b977e5c51404b0490059e202bc
Hung <jimmyhunganh2k@gmail.com>
2024-10-11 11:41:14 +0700

    mgk

--- views/orderhistory/orderhistory.js
+++ views/orderhistory/orderhistory.js
@@ -1,19 +1,31 @@
 app.controller("orderhistory", function ($scope, $http, $location, $filter) {
-    $scope.orders = {};
-
+    $scope.orders = [];
+    $scope.orderDetails = [];
+    $scope.viewOrder = {};
     
     $scope.initialize = function () {
         $http.get("http://localhost:8000/api/client/orders").then(resp => {
             $scope.orders = resp.data;
-            console.log($scope.orders);
+            $scope.viewSingleOrder($scope.orders[0]);
+
         }).catch(error => {
             console.log("Error", error);
         })
     }
 
 
+    $scope.viewSingleOrder = function (order) {
+        $scope.viewOrder = order;
+    }
 
+    $scope.viewOrderDetail = function (id) {
+        $http.get("http://localhost:8000/api/client/order-details").then(resp => {
+           
 
+        }).catch(error => {
+            console.log("Error", error);
+        })
+    }
 
 
     $scope.initialize();
