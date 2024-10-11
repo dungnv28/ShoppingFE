6fdec95842ea6f545edcdd2d49b84ff2c4f366a8
Hung <jimmyhunganh2k@gmail.com>
2024-10-11 16:18:03 +0700

    g

--- views/orderhistory/orderhistory.js
+++ views/orderhistory/orderhistory.js
@@ -7,7 +7,7 @@ app.controller("orderhistory", function ($scope, $http, $location, $filter) {
         $http.get("http://localhost:8000/api/client/orders").then(resp => {
             $scope.orders = resp.data;
             $scope.viewSingleOrder($scope.orders[0]);
-
+            $scope.viewOrderDetail($scope.orders[0].id);
         }).catch(error => {
             console.log("Error", error);
         })
@@ -15,13 +15,13 @@ app.controller("orderhistory", function ($scope, $http, $location, $filter) {
 
 
     $scope.viewSingleOrder = function (order) {
+        $scope.viewOrderDetail(order.id);
         $scope.viewOrder = order;
     }
 
     $scope.viewOrderDetail = function (id) {
-        $http.get("http://localhost:8000/api/client/order-details").then(resp => {
-           
-
+        $http.get("http://localhost:8000/api/client/order-details/getbyorderid/"+id).then(resp => {
+            $scope.orderDetails = resp.data;
         }).catch(error => {
             console.log("Error", error);
         })
@@ -29,4 +29,7 @@ app.controller("orderhistory", function ($scope, $http, $location, $filter) {
 
 
     $scope.initialize();
+    
+    $scope.isCollapsed = true;
+    
 });
