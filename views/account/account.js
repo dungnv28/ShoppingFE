db6dc882e45b801342e5723288e203ddf70751cb
hungNumNum <jimmyhunganh2k@gmail.com>
2024-10-26 20:04:37 +0700

    mgk

--- views/account/account.js
+++ views/account/account.js
@@ -1,6 +1,7 @@
 app.controller("account", function ($scope, $http, $location, $filter) {
     $scope.searchTerm = '';
     $scope.accounts = [];
+    $scope.userRole = [];
 
     $scope.initialize = function () {
         $http.get("http://localhost:8000/api/client/accounts").then(resp => {
@@ -11,10 +12,17 @@ app.controller("account", function ($scope, $http, $location, $filter) {
     }
 
 
+    $scope.findRoleByAccountId = function (id) {
+        $http.get("http://localhost:8000/api/admin/userroles/getrole/"+id).then(resp => {
+            $scope.userRole = resp.data;
+            console.log($scope.userRole)
+        }).catch(error => {
+            console.log("Error", error);
+        })
+    }
 
 
-
-
+    $scope.findRoleByAccountId(1)
     $scope.initialize();
 
 });
