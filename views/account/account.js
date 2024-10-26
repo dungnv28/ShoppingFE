5bcd8b75b7fd1b16aea3c9056b007bea2b3e4d05
hungNumNum <jimmyhunganh2k@gmail.com>
2024-10-26 19:10:12 +0700

    mgk

--- views/account/account.js
+++ views/account/account.js
@@ -1,11 +1,20 @@
 app.controller("account", function ($scope, $http, $location, $filter) {
+    $scope.searchTerm = '';
+    $scope.accounts = [];
+
+    $scope.initialize = function () {
+        $http.get("http://localhost:8000/api/client/accounts").then(resp => {
+            $scope.accounts = resp.data;
+        }).catch(error => {
+            console.log("Error", error);
+        })
+    }
 
 
 
 
 
 
-
-
+    $scope.initialize();
 
 });
