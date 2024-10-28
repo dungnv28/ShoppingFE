c37368f59f97a86e54e0e578f9a5b41986805553
hungNumNum <jimmyhunganh2k@gmail.com>
2024-10-28 19:38:27 +0700

    mgk

--- views/product/product.js
+++ views/product/product.js
@@ -1,10 +1,18 @@
 app.controller("product", function ($scope, $http, $location, $filter) {
     $scope.searchTerm = '';
     $scope.products = [];
+    $scope.categories = [];
     $scope.product = {};
     $scope.initialize = function () {
         $http.get("http://localhost:8000/api/admin/products").then(resp => {
             $scope.products = resp.data;
+        }).catch(error => {
+            console.log("Error", error);
+        })
+
+        $http.get("http://localhost:8000/api/admin/categories").then(resp => {
+            $scope.categories = resp.data;
+            
         }).catch(error => {
             console.log("Error", error);
         })
