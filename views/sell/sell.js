c89228e87a9fed4e3b2296cd411b0def8a5ed195
Hung <jimmyhunganh2k@gmail.com>
2024-10-31 11:53:49 +0700

    mgk

--- views/sell/sell.js
+++ views/sell/sell.js
@@ -1,22 +1,32 @@
-app.controller("sell", function ($scope, $http) {
+app.controller("sell", function ($scope, $http,authService,$location) {
     $scope.products = [];
     $scope.listItems = [];
     $scope.accounts = [];
     $scope.account = {};
+    $scope.acc = {};
+    $scope.order = {};
+    $scope.form = { vanglai: false };
+
+     // khách vãng lai , check nếu là true thì đơn hàng sẽ là khách vãng lại => clear data của $scope.account
     $scope.initialize = function () {
-        $http.get("http://localhost:8000/api/admin/products").then(resp => {
-            $scope.products = resp.data;
-        }).catch(error => {
-            console.log("Error", error);
-        })
-
-        $http.get("http://localhost:8000/api/client/accounts").then(resp => {
-            $scope.accounts = resp.data;
-        }).catch(error => {
-            console.log("Error", error);
-        })
-
-        
+        if (authService.getToken()) {
+            $http.get("http://localhost:8000/api/client/accounts/" + authService.getUsername()).then(resp => {
+                $scope.acc = resp.data;
+            });
+            $http.get("http://localhost:8000/api/admin/products").then(resp => {
+                $scope.products = resp.data;
+            }).catch(error => {
+                console.log("Error", error);
+            })
+    
+            $http.get("http://localhost:8000/api/client/accounts").then(resp => {
+                $scope.accounts = resp.data;
+            }).catch(error => {
+                console.log("Error", error);
+            })
+        } else {
+            $location.path('/login');
+        }
     }
 
     $scope.addItem = function(item) {
@@ -39,7 +49,6 @@ app.controller("sell", function ($scope, $http) {
     
     $scope.addAccount = function(account) {
         $scope.account = account; 
-        console.log($scope.account)
     };
     
     $scope.TotalPrice = function() {
@@ -58,6 +67,55 @@ app.controller("sell", function ($scope, $http) {
         $scope.account = {};
     };
 
+
+    // ------------- order --------------
+    $scope.sold = function () {
+        $scope.order.amount = $scope.TotalPrice();
+        $scope.order.status = 2;
+        if (Object.keys($scope.account).length === 0 && !$scope.form.vanglai) { 
+            alert("Vui lòng chọn khách hàng!");
+        }
+        else if($scope.form.vanglai){
+            $scope.order.account = $scope.acc
+        }else {
+            $scope.order.account = $scope.account
+        }
+        console.log($scope.order.account)
+
+        // $scope.order.account = $scope.account; // cho thêm if else nếu là khách vãng lai
+        // $scope.order.code = $scope.generateCode(authService.getUsername()); //code ở dạng auto gen phía back-end
+        // $http.post("http://localhost:8000/api/client/orders", $scope.order)
+        //     .then(resp => {
+        //         console.log("Đơn hàng đã được tạo thành công!", resp.data);
+        //         const orderId = resp.data.id;
+        //         const createOrderDetailPromises = $scope.carts.map(cart => {
+        //             const orderDetail = {
+        //                 order: { id: orderId },
+        //                 product: { id: cart.product.id },
+        //             };
+        //             return $http.post("http://localhost:8000/api/client/order-details", orderDetail);
+        //         });
+        //         return Promise.all(createOrderDetailPromises);
+        //     })
+        //     .then(responses => {
+        //         console.log("Chi tiết đơn hàng đã được tạo thành công!", responses);
+        //         $scope.loading = false;
+        //         $('#checkout').modal('hide');
+        //         $scope.deleteCartsByAccount($scope.account.id);
+        //         $scope.carts = [];
+        //     })
+        //     .catch(error => {
+        //         console.log("Lỗi khi tạo đơn hàng", error);
+        //         $scope.loading = false;
+        //     });
+    };
+
+    $scope.generateCode = function(username) {
+        var timestamp = Date.now(); 
+        var randomString = Math.random().toString(36).substring(2, 8);
+        var generatedCode = randomString + timestamp + "_" + username;
+        return generatedCode;
+    };
 
 
     $scope.initialize();
